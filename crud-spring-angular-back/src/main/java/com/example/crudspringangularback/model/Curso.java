77a5ff39d8131e8c25b8f169a82df1fa80b38267
arnaldorcm <arnaldo1ooo95@gmail.com>
2022-05-30 19:38:48 -0400

    Se añadieron ajustes al modelo de Curso, se añadio properties de Spring

--- crud-spring-angular-back/src/main/java/com/example/crudspringangularback/model/Curso.java
+++ crud-spring-angular-back/src/main/java/com/example/crudspringangularback/model/Curso.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -18,11 +19,12 @@ public class Curso {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonProperty("_id")	//Cuando genere de Objeto a Json al enviar al front, enviara con este nombre
 	private Long id;
 	
-	@Column(name="cur_nombre")
+	@Column(name="cur_nombre", length = 200, nullable = false)
 	private String nombre;
 	
-	@Column(name="cur_categoria")
+	@Column(name="cur_categoria", length = 200, nullable = false)
 	private String categoria;
 }
